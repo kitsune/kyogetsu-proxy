f39d9445c38eb1e9c0954e842ba5ef67c87fcc5b
Dylan Enloe <enloed@gmail.com>
2017-10-10 16:54:51 -0700

    Increase the data in a message
    Added the Full Response for both Production and Staging to Message
    Added the Staging Request to Message
    Added Header to Request info

--- kyogetsu/message.go
+++ kyogetsu/message.go
@@ -4,6 +4,7 @@ package kyogetsu
 
 import (
   "net/http"
+  "net/http/httptest"
   "io/ioutil"
 )
 
@@ -13,28 +14,46 @@ type MessageSender interface {
 
 type RequestInfo struct {
   Method string
+  URI string
+  Header http.Header
   Body string
-  URL string
+}
 
+type ResponseInfo struct {
+  Status int
+  Header http.Header
+  Body string
 }
 
 type Message struct {
-  Request RequestInfo
-  ProductionReponse string
-  StagingReponse string
+  ProdRequest RequestInfo
+  StagingRequest RequestInfo
+  ProdReponse ResponseInfo
+  StagingReponse ResponseInfo
 }
 
 //NewRequestInfo generates the proper RequestInfo for
 //the given http.Request
 func NewRequestInfo(r *http.Request) RequestInfo {
-  b, err := ioutil.ReadAll(r.Body);
+  b, err := ioutil.ReadAll(r.Body)
   if  err != nil {
-    return RequestInfo{Method: r.Method, URL: r.URL.String()}
+    return RequestInfo{Method: r.Method, URI: r.URL.String(), Header: r.Header}
   }
-  return RequestInfo{Method: r.Method, Body: string(b), URL: r.URL.String()}
+  return RequestInfo{Method: r.Method, URI: r.URL.String(), Header: r.Header, Body: string(b)}
+}
+
+//NewResponseInfo generates the proper ResponseInfo for
+//the given httptest.ResponseRecorder
+func NewResponseInfo(r *httptest.ResponseRecorder) ResponseInfo {
+  return ResponseInfo{Status: r.Code, Header: r.Header(), Body: r.Body.String()}
 }
 
 //NewMessage creates a new message from the given data
-func NewMessage(p string, s string, r *http.Request) *Message {
-  return &Message{Request: NewRequestInfo(r), ProductionReponse: p, StagingReponse: s}
+func NewMessage(p *httptest.ResponseRecorder, s *httptest.ResponseRecorder,
+      pr *http.Request, sr *http.Request) *Message {
+  return &Message{
+    ProdRequest: NewRequestInfo(pr),
+    StagingRequest: NewRequestInfo(sr),
+    ProdReponse: NewResponseInfo(p),
+    StagingReponse: NewResponseInfo(s)}
 }
--- kyogetsu/message_test.go
+++ kyogetsu/message_test.go
@@ -5,8 +5,10 @@ package kyogetsu
 import (
   "errors"
   "net/http"
+  "net/http/httptest"
   "strings"
   "testing"
+  "fmt"
   )
 
 type failReader struct {
@@ -17,36 +19,74 @@ func (r failReader) Read(b []byte) (n int, err error) {
   return 0, errors.New("Read Error")
 }
 
+func verifyHeader(t *testing.T, hi http.Header, h http.Header) {
+  if len(hi) != len(h) {
+    t.Errorf("Header lengths do not match. Expected: %s Got: %s", len(h), len(hi))
+    fmt.Println(hi)
+    return
+  }
+  for k, v := range hi {
+    if len(v) != len(h[k]) {
+      t.Errorf("Value lengths for header key %s do not match. Expected: %s Got: %s",
+               k,
+               len(h[k]),
+               len(v))
+      return
+    }
+    for i, val := range v {
+      if val != h[k][i] {
+        t.Errorf("Header field doesn't match. Expected: %s Got: %s", h[k][i], val)
+      }
+    }
+  }
+}
+
 func verifyRequestInfo(t *testing.T, ri RequestInfo, r RequestInfo) {
   if ri.Method != r.Method {
     t.Errorf("Method doesn't match. Expected: %s Got: %s", r.Method, ri.Method)
   }
-  if ri.URL != r.URL {
-    t.Errorf("URL doesn't match. Expected: %s Got: %s", r.URL, ri.URL)
+  if ri.URI != r.URI {
+    t.Errorf("URI doesn't match. Expected: %s Got: %s", r.URI, ri.URI)
   }
   if ri.Body != r.Body {
-    t.Errorf("Method doesn't match. Expected: %s Got: %s", r.Body, ri.Body)
+    t.Errorf("Body doesn't match. Expected: %s Got: %s", r.Body, ri.Body)
+  }
+  verifyHeader(t, ri.Header, r.Header)
+}
+
+func verifyResponseInfo(t *testing.T, ri ResponseInfo, r ResponseInfo) {
+  if ri.Status != r.Status {
+    t.Errorf("Respose Status doesn't match. Expected: %s Got: %s", r.Status, ri.Status)
+  }
+  if ri.Body != r.Body {
+    t.Errorf("Respose Body doesn't match. Expected: %s Got: %s", r.Body, ri.Body)
   }
 }
 
 func verifyMessage(t *testing.T, msg *Message, m Message) {
-  if msg.ProductionReponse != m.ProductionReponse {
-    t.Errorf("ProductionReponse doesn't match. Expected: %s Got: %s", m.ProductionReponse, msg.ProductionReponse)
-  }
-  if msg.StagingReponse != m.StagingReponse {
-    t.Errorf("StagingReponse doesn't match. Expected: %s Got: %s", m.StagingReponse, msg.StagingReponse)
-  }
-  verifyRequestInfo(t, msg.Request, m.Request)
+  verifyResponseInfo(t, msg.ProdReponse, m.ProdReponse)
+  verifyResponseInfo(t, msg.StagingReponse, m.StagingReponse)
+  verifyRequestInfo(t, msg.ProdRequest, m.ProdRequest)
+  verifyRequestInfo(t, msg.StagingRequest, m.StagingRequest)
+}
+
+func makeResponse(t *testing.T, r *ResponseInfo) *httptest.ResponseRecorder{
+  res := httptest.NewRecorder()
+  res.HeaderMap = r.Header
+  res.WriteString(r.Body)
+  res.Code = r.Status
+  return res
 }
 
 func TestNewRequestInfo(t *testing.T) {
   var tests = []RequestInfo {
-      {"POST", "This is a test", "example.com"},
-      {"GET", "this is also a test", "test.com"},
+      {"POST", "example.com", http.Header{"Cookie": {"A Cookie"}}, "This is a test"},
+      {"GET", "test.com", http.Header{"Cookie": {"Session"}}, "this is also a test"},
     }
   for _, test := range tests {
     b := strings.NewReader(test.Body)
-    r, _ := http.NewRequest(test.Method, test.URL, b)
+    r, _ := http.NewRequest(test.Method, test.URI, b)
+    r.Header = test.Header
     ri := NewRequestInfo(r)
     verifyRequestInfo(t, ri, test)
   }
@@ -54,11 +94,12 @@ func TestNewRequestInfo(t *testing.T) {
 
 func TestNewRequestInfoWithoutBody(t *testing.T) {
   var tests = []RequestInfo {
-      {"POST", "", "example.com"},
-      {"GET", "", "test.com"},
+      {"POST", "example.com", http.Header{"Cookie": {""}}, ""},
+      {"GET", "test.com", http.Header{"Cookie": {"cat", "dog"}}, ""},
     }
   for _, test := range tests {
-    r, _ := http.NewRequest(test.Method, test.URL, failReader{})
+    r, _ := http.NewRequest(test.Method, test.URI, failReader{})
+    r.Header = test.Header
     ri := NewRequestInfo(r)
     verifyRequestInfo(t, ri, test)
   }
@@ -66,13 +107,29 @@ func TestNewRequestInfoWithoutBody(t *testing.T) {
 
 func TestNewMessage(t *testing.T) {
   var tests = []Message {
-      {RequestInfo{"POST", "This is a test", "example.com"}, "Prod", "Test"},
-      {RequestInfo{"GET", "this is also a test" , "test.com"}, "", ""},
+      {
+        RequestInfo{"POST", "example.com", http.Header{"Cookie": {"Prod Request"}}, "This is a prod test"},
+        RequestInfo{"POST", "example.com", http.Header{"Cookie": {"Staging Resquest"}}, "This is a staging test"},
+        ResponseInfo{301, http.Header{"Cookie": {"Prod Response"}}, "Prod"},
+        ResponseInfo{302, http.Header{"Cookie": {"Staging Response"}}, "Test"},
+      },
+      {
+        RequestInfo{"GET", "test.com", http.Header{}, "this is also a prod test" },
+        RequestInfo{"GET", "test.com", http.Header{"Size": {"Not Empty"}}, "this is also a staging test" },
+        ResponseInfo{200, http.Header{"Cookie": {"A"}}, ""},
+        ResponseInfo{404, http.Header{"Cookie": {"A"}}, ""},
+      },
     }
   for _, test := range tests {
-    b := strings.NewReader(test.Request.Body)
-    r, _ := http.NewRequest(test.Request.Method, test.Request.URL, b)
-    m := NewMessage(test.ProductionReponse, test.StagingReponse, r)
+    b := strings.NewReader(test.ProdRequest.Body)
+    pr, _ := http.NewRequest(test.ProdRequest.Method, test.ProdRequest.URI, b)
+    b = strings.NewReader(test.StagingRequest.Body)
+    pr.Header = test.ProdRequest.Header
+    sr, _ := http.NewRequest(test.StagingRequest.Method, test.StagingRequest.URI, b)
+    sr.Header = test.StagingRequest.Header
+    p := makeResponse(t, &test.ProdReponse)
+    s := makeResponse(t, &test.StagingReponse)
+    m := NewMessage(p, s, pr, sr)
 
     verifyMessage(t, m, test)
   }
